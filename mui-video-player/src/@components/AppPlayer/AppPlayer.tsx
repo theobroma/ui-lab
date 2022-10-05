1c505db7046c833935cdf191fb508fc540e60ab1
Alexandr Siryj <theobroma222@gmail.com>
2022-10-05 19:45:14 +0300

    AppPlayer FC

---
+++ mui-video-player/src/@components/AppPlayer/AppPlayer.tsx
@@ -0,0 +1,9 @@
+const AppPlayer = () => {
+  return (
+    <header className="header__section">
+      <p className="header__text">Build a Video Player - Tutorial</p>
+    </header>
+  );
+};
+
+export default AppPlayer;
--- mui-video-player/src/@views/MainView.tsx
+++ mui-video-player/src/@views/MainView.tsx
@@ -1,25 +1,12 @@
-import { Box, Grid } from '@mui/material';
+import { Grid } from '@mui/material';
 
-import logo from '../@assets/images/react-mui.png';
+import AppPlayer from '../@components/AppPlayer';
 
 const MainView = () => {
   return (
     <Grid container spacing={2}>
-      <Grid item xs={2}>
-        {/* xs=2 */}
-      </Grid>
-      <Grid item xs={8}>
-        <Box
-          style={{
-            display: 'flex',
-            justifyContent: 'center',
-          }}
-        >
-          <img src={logo} alt="Logo" />
-        </Box>
-      </Grid>
-      <Grid item xs={2}>
-        {/* xs=2 */}
+      <Grid item xs={12}>
+        <AppPlayer />
       </Grid>
     </Grid>
   );
