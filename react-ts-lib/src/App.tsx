55acde1f67aab48df1fbb71c865b6e6e4d308728
Alexandr Siryj <theobroma222@gmail.com>
2022-10-08 23:38:26 +0300

    Button component

--- react-ts-lib/src/App.tsx
+++ react-ts-lib/src/App.tsx
@@ -1,7 +1,44 @@
-import "./App.css";
+import { Button, BUTTON_VARIANTS } from "./lib";
 
 function App() {
-    return <div className="App">btn</div>;
+    // обработчик нажатия кнопки
+    // принимает вариант кнопки
+    const onClick = (variant: string) => {
+        // выводим сообщение в консоль инструментов разработчика в браузере
+        console.log(`${variant} button clicked`);
+    };
+
+    return (
+        <>
+            {/* дефолтная кнопка */}
+            <Button onClick={() => onClick("primary")}>primary</Button>
+            {/* заблокированная кнопка */}
+            <Button onClick={() => onClick("disabled")} disabled>
+                disabled
+            </Button>
+            {/* успех */}
+            <Button
+                variant={BUTTON_VARIANTS.SUCCESS}
+                onClick={() => onClick(BUTTON_VARIANTS.SUCCESS)}
+            >
+                {BUTTON_VARIANTS.SUCCESS}
+            </Button>
+            {/* предупреждение */}
+            <Button
+                variant={BUTTON_VARIANTS.WARNING}
+                onClick={() => onClick(BUTTON_VARIANTS.WARNING)}
+            >
+                {BUTTON_VARIANTS.WARNING}
+            </Button>
+            {/* опасность */}
+            <Button
+                variant={BUTTON_VARIANTS.DANGER}
+                onClick={() => onClick(BUTTON_VARIANTS.DANGER)}
+            >
+                {BUTTON_VARIANTS.DANGER}
+            </Button>
+        </>
+    );
 }
 
 export default App;
---
+++ react-ts-lib/src/lib/Button/Button.tsx
@@ -0,0 +1,76 @@
+// https://stackoverflow.com/questions/59048346/extending-component-props-to-default-html-button-props-correctly
+// https://stackoverflow.com/questions/54383386/react-typescript-correct-type-for-event-handler-prop
+import {
+    ButtonHTMLAttributes,
+    FC,
+    MouseEventHandler,
+    PropsWithChildren,
+} from "react";
+import styled from "styled-components";
+
+export enum BUTTON_VARIANTS {
+    PRIMARY = "primary",
+    SUCCESS = "success",
+    WARNING = "warning",
+    DANGER = "danger",
+}
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
+    variant?: BUTTON_VARIANTS;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+};
+
+const Button: FC<PropsWithChildren<Props>> = ({
+    children,
+    disabled,
+    onClick,
+    variant = BUTTON_VARIANTS.PRIMARY,
+    ...restProps
+}) => {
+    // если кнопка заблокирована, переданный обработчик не вызывается
+    const handleClick: MouseEventHandler<HTMLButtonElement> = (e) => {
+        if (disabled) return;
+        onClick && onClick(e); // ???
+    };
+
+    return (
+        <button disabled={disabled} onClick={handleClick} {...restProps}>
+            {children}
+        </button>
+    );
+};
+
+const StyledButton = styled(Button)`
+    background-color: var(
+        --${(props) => (props.disabled ? "gray" : props.variant ?? "primary")}
+    );
+    border-radius: 6px;
+    border: none;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.4);
+    color: var(
+        ${(props) =>
+            props.variant &&
+            (props.variant === BUTTON_VARIANTS.SUCCESS ??
+                props.variant === BUTTON_VARIANTS.WARNING)
+                ? "--dark"
+                : "--light"}
+    );
+    cursor: ${(props) => (props.disabled ? "default" : "pointer")};
+    font-weight: 600;
+    letter-spacing: 1px;
+    opacity: ${(props) => (props.disabled ? "0.6" : "1")};
+    outline: none;
+    padding: 0.8rem;
+    text-transform: uppercase;
+    transition: 0.4s;
+
+    &:not([disabled]):hover {
+        opacity: 0.8;
+    }
+
+    &:active {
+        box-shadow: none;
+    }
+`;
+
+export default StyledButton;
